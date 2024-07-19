ea1cbde4d9fb9960cf52d054709d634a227dd2f5
cw <fromnowwon@gmail.com>
2024-07-19 20:30:45 +0900

    feat: home page 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { QueryClientProvider, QueryClient } from "react-query";
+import Home from "./pages/Home";
+
 function App() {
-  return <div></div>;
+  const queryClient = new QueryClient();
+
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    </div>
+  );
 }
 
 export default App;
