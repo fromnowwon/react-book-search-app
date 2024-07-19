2413b1639afe62861fa69db0af45b82ac92d2d56
cw <fromnowwon@gmail.com>
2024-07-19 21:13:37 +0900

    feat: page size 추가

--- src/api/books.ts
+++ src/api/books.ts
@@ -1,12 +1,15 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
-const MAX_RESULTS = 10;
 
-export const fetchBooks = async (query: string, startIndex: number = 0) => {
+export const fetchBooks = async (
+  query: string,
+  startIndex: number = 0,
+  pageSize: number = 10
+) => {
   const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&startIndex=${startIndex}&maxResults=${MAX_RESULTS}`
+    `${BASE_URL}?key=${API_KEY}&q=${query}&startIndex=${startIndex}&maxResults=${pageSize}`
   );
   const data = await response.json();
-
+  console.log(data);
   return data;
 };
--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,10 +1,14 @@
 import { useQuery } from "react-query";
 import { fetchBooks } from "../api/books";
+import { useBookStore } from "../store/useBookStore";
 
-export const useBooks = (query: string, startIndex: number) => {
+export const useBooks = (query: string, pageIndex: number) => {
+  const { pageSize } = useBookStore();
+  const startIndex = pageIndex * pageSize;
+
   return useQuery(
-    ["books", query, startIndex],
-    () => fetchBooks(query, startIndex),
-    { enabled: !!query, keepPreviousData: true }
+    ["books", query, startIndex, pageSize],
+    () => fetchBooks(query, startIndex, pageSize),
+    { initialData: { items: [] }, enabled: !!query, keepPreviousData: true }
   );
 };
